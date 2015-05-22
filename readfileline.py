fb5fd6fa3dfd41b0d0663efe542234bb4d7888ae
Milind <milind@GreenMeadow.local>
2015-05-22 13:36:40 -0700

    added minor comment

--- readfileline.py
+++ readfileline.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 #Useful to read file line by line, can be redirected to another file or program as input
+#The whole file can be read in one go, but it takes time to get output lines if file is huge
 
 with open('error_log_union') as f:
     for line in f:
